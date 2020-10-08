8e14cd7b07682750d0ff174be5c12073fb971050
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-08 17:34:28 -0600

    update travis URL

--- scripts/pre-push.js
+++ scripts/pre-push.js
@@ -10,8 +10,8 @@ try {
     console.log(
       `You're trying to push to Kent's repo directly. If you want to save and push your work or even make a contribution to the workshop material, you'll need to fork the repo first and push changes to your fork. Learn how here: https://docs.github.com/en/free-pro-team@latest/github/getting-started-with-github/fork-a-repo`,
     )
+    process.exit(1)
   }
-  process.exit(1)
 } catch (error) {
   // ignore
 }
