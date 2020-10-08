8bbf73d798467c696f858a1955bf80fcaac2b9a7
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-08 10:34:13 -0600

    tmp

--- scripts/pre-push.js
+++ scripts/pre-push.js
@@ -1,5 +1,5 @@
-var spawnSync = require('child_process').spawnSync
-const {username} = require('os').userInfo()
+// var spawnSync = require('child_process').spawnSync
+// const {username} = require('os').userInfo()
 
 console.log(process.env)
 
