f4573e9a3a8004bbae1b639e1639c90da7b0a9a7
Art Telesh <35339760+decisa@users.noreply.github.com>
2020-10-08 23:43:22 -0400

    added name attribute to input element (#96)
    * added name attribute to input element
    
    * updated Kody the Koala's notes
    
    * placed name attribute in the right place

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -16,13 +16,14 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 add the onSubmit handler to the <form> below
 
-  // 🐨 make sure to associate the label to the input by specifying an `id` on
-  // the input and a matching value as an `htmlFor` prop on the label.
+  // 🐨 replace input's name attribute with id attribute
+  // 🐨 make sure to associate the label to the input. 
+  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
     <form>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input name="username" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
