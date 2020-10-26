7c0ddff0c2bdb42375770a19c14bd102ebb913b6
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-26 09:38:40 -0600

    remove name from input and let them set that themselves

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -16,14 +16,13 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 add the onSubmit handler to the <form> below
 
-  // 🐨 replace input's name attribute with id attribute
   // 🐨 make sure to associate the label to the input. 
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
     <form>
       <div>
         <label>Username:</label>
-        <input name="username" type="text" />
+        <input type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
