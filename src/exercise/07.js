56b16e689fde6b0da2921912b6988305d45597a1
Mouhcine Daali <daalim277@gmail.com>
2022-04-01 09:24:20 +0000

    compleet the exercise

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -29,8 +29,7 @@ function App() {
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
