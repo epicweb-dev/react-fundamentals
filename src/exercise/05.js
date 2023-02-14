19a42b2d6c2988a87963d8d463cd3de9fe59114e
Stefano Ramirez <stefanoramirez@Stefanos-MacBook-Air.local>
2023-02-13 19:29:40 -0600

    Actualizando el branch con progreso Modulo 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,10 +14,79 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <div 
+  className= "box box--small" // using different class names overloads more CSS on the same elements
+  style = {{backgroundColor: "lightblue", fontStyle: "italic"}}
+  >
+  small lightblue box
+  </div>
 
+const mediumBox = <div
+  className= "box box--medium"
+  style = {{backgroundColor: "pink", fontStyle: "italic"}}
+  >
+    medium pink box
+</div>
+
+const largeBox = <div
+  className = "box box--large"
+  style = {{backgroundColor:"orange", fontStyle: "italic"}} 
+ >large orange box</div>
+
+
+//extra credit 1 making a costum Box component
+/*
+function Box({style, className ='', ...otherProps}){
+  return (
+    <div 
+      className = {`box ${className}`}
+      style = {{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+*/
+
+//override of the Box function taking in the size parameter.
+function Box({style, size, className, ...otherProps}){
+  const sizeName = size ? `box--${size}` : '' // is there a size operand? if so set to return (
+    return(
+      <div 
+      className = {`box ${sizeName}`}
+      style = {{fontStyle: 'italic', ...style}}
+      {...otherProps}
+
+      />
+    )
+}
+
+//extra credit 2 implementation with size parameter in API
+ function App(){
+  return <div>
+    <Box size="small" style={{backgroundColor: 'lightblue'}}>
+      small lightblue box
+    </Box>
+
+    <Box size="medium" style={{backgroundColor: 'pink'}}>
+      medium pink box
+    </Box>
+
+    <Box size="large" style={{backgroundColor: 'orange'}}>
+      large orange box
+    </Box>
+
+    <Box>
+      sizeless box
+    </Box>
+
+  </div>
+ }
+ //App() running with the latest implementation of the box component that takes in the 
+ //small, medium and large options and hard codes that operand thus selecting the desired CSS option.
+
+ //remember to use the JSX expression of the tag e.g. <div /> within the box funtion as the returning React element.
+
+/*
 function App() {
   return (
     <div>
@@ -27,5 +96,35 @@ function App() {
     </div>
   )
 }
+*/
+
+/*function App(){
+  //the class name must be in "" not '' or else it wont compile well.
+  return (
+    <div>
+      <Box className = "box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+
+      <Box className = "box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+
+      <Box className = "box--large" style={{backgroundColor:'orange'}}>
+        large orange box
+      </Box>
+
+      <Box>
+        sizeless box
+      </Box>
+    </div>
+  )
+}
+*/
+
+//working implementation, forgot the <div> element to group the boxes, was offered to use 
+//a fragment, <></> 
+
+//re-implementing with the costum made box
 
 export default App
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,7 +4,31 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  //changing to username and eliminating the error implementation.
+  //const [error, setError]= React.useState(null)
+  const [username, setUsername] = React.useState('') //using '' instead of null.
+
+  //const inputRef = React.useRef()
+  
   // 🐨 add a submit event handler here (`handleSubmit`).
+  function handleSubmit (event){
+
+    //calling the event.preventDefault() //refresh the page is default.
+    event.preventDefault()
+
+    //onSubmitUsername(event.target.elements.usernameInput.value)
+    //variation.
+    
+    //onSubmitUsername(event.target.elements[0].value) //working but turning off for extra credit.
+
+    //now implementing the extra credit variation to get the value by using a ref.
+    //const inputRef = React.inputRef()//current.value
+    //onSubmitUsername(inputRef.current.value) //implementation of extra credit 2
+    onSubmitUsername(username)
+
+    //sticking with the argument value val for now. 3 
+
+  }
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -19,12 +43,38 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  /*
+function handleChange(event){
+  const {value} = event.target
+  const isLowerCase = (value === value.toLowerCase()) //ask ed for the tripple use operator.
+  setError(isLowerCase ? null : 'Username must be lower case') // null gives an empty space in the html.
+}
+*/
+
+//extra credit 3 implementation with no error catching and controlled input.
+function handleChange(event){
+  //setUsername(inputRef.current.value.toLowerCase()) //this controls but not real time.
+  setUsername(event.target.value.toLowerCase())
+}
+
+
+
+  //the onSubmit is used in the form tag, and the curly brackets for the handleSubmit function.
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}> 
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input 
+          id="usernameInput" 
+          type="text" 
+          //ref={inputRef} 
+          onChange={handleChange}
+          value={username} //was missing this prop this enabled the controlled varaable to work in real time.
+        />
       </div>
+
       <button type="submit">Submit</button>
     </form>
   )
