729cad742b569d8db023f9a21707de3002c0c2f2
Stefano Ramirez <stefanoramirez@Stefanos-MacBook-Air.local>
2023-02-17 12:40:30 -0600

    Testing the new key prop

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -30,7 +30,10 @@ function App() {
       <ul>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          
+          //added the key prop to the item.id
+
+          <li key = {item.id} >
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
