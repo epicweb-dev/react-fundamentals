efd864d4ba58e9f398d471287162d10d7d1b10fc
Harshit Sinha <harshit.sinha@sprinklr.com>
2023-02-23 06:57:41 +0000

    completed till module1 exercise6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,30 +4,31 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const [username, setUsername] = React.useState('')
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const inputRef = React.useRef(null)
+  function handleSubmit(event) {
+    event.preventDefault()
+    let username = inputRef.current.value
+    onSubmitUsername(username)
+  }
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label type="username">Username:</label>
+        <input
+          type="text"
+          name="usernameInput"
+          ref={inputRef}
+          value={'harshit'}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
   )
+  // 🐨 make sure to associate the label to the input.
+  // to do so, set the value of 'htmlFor' prop of the label to the id of input
 }
 
 function App() {
