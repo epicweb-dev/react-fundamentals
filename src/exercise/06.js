ec648459ba1c583c0fb2753e6c85e8efc74f32fe
navsi100rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-14 13:29:55 +0300

    Complete exercise 6.2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,11 +1,11 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import * as React from "react";
 
-function UsernameForm({onSubmitUsername}) {
-
-  const inputRef = React.useRef('');
+function UsernameForm({ onSubmitUsername }) {
+  const inputRef = React.useRef("");
+  const [error, setError] = React.useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -13,20 +13,41 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(username);
   }
 
+  function handleChange({ target }) {
+    const { value } = target;
+    const isValid = value === value.toLowerCase();
+    setError(isValid ? null : "Username must be lowercase");
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor='username'>Username:</label>
-        <input ref={inputRef} id="username" name='username' type="text" />
+        <label htmlFor="username">Username:</label>
+        <input
+          onChange={handleChange}
+          ref={inputRef}
+          id="username"
+          name="username"
+          type="text"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <div role="alert" style={{ color: "red" }}>
+        {error}
+      </div>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
     </form>
-  )
+  );
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  const onSubmitUsername = (username) => alert(`You entered: ${username}`);
+  return (
+    <div style={{minWidth: 400}}>
+      <UsernameForm onSubmitUsername={onSubmitUsername} />
+    </div>
+  );
 }
 
-export default App
+export default App;
