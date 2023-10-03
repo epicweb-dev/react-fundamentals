f8ebfab8b5de0c5e8284ac7c88c12b877959ec49
Michael Chan <mijoch@gmail.com>
2023-10-03 13:48:44 -0700

    Migrate Lesson 3 to workshop-app (no mat. change)

--- exercises/05.typescript-with-react/01.solution/types.test.tsx
+++ exercises/05.typescript-with-react/01.solution/types.test.tsx
@@ -1,15 +1,19 @@
+import {waitFor, within} from '@testing-library/react'
 import {expect, testStep} from '@kentcdodds/workshop-app/test'
 import '.'
 
-await testStep('a', async () => {
-  const codeStrings = Array.from(document.body.querySelectorAll('code')).map(
-    c => c.textContent,
-  )
+const body = within(document.body)
 
-  expect(codeStrings.join('\n')).toMatchInlineSnapshot(`
-    "1 + 2 = 3
-    1 - 2 = -1
-    1 * 2 = 2
-    1 / 2 = 0.5"
-  `)
+await testStep('a', async () => {
+  expect(await body.findByText('1 + 2 = 3')).to.exist
+  // const codeStrings = Array.from(body.queryAllBy('code')).map(
+  //   c => c.textContent,
+  // )
+
+  // expect(codeStrings.join('\n')).toMatchInlineSnapshot(`
+  //   "1 + 2 = 3
+  //   1 - 2 = -1
+  //   1 * 2 = 2
+  //   1 / 2 = 0.5"
+  // `)
 })
