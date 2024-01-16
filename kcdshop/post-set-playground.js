c6d665e0518af414163115b06beba70850833e09
Kent C. Dodds <me+github@kentcdodds.com>
2024-01-16 16:35:33 -0700

    use global kcdshop

--- kcdshop/post-set-playground.js
+++ kcdshop/post-set-playground.js
@@ -5,7 +5,3 @@ fs.writeFileSync(
 	path.join(process.env.KCDSHOP_PLAYGROUND_DEST_DIR, 'tsconfig.json'),
 	JSON.stringify({ extends: '../tsconfig' }, null, 2),
 )
-fs.rmSync(path.join(process.env.KCDSHOP_PLAYGROUND_DEST_DIR, 'kcdshop'), {
-	recursive: true,
-	force: true,
-})
