2c5df4b7000b65dde5c2eebaa9b6b819a2f1ef9f
Kent C. Dodds <me+github@kentcdodds.com>
2024-01-16 16:38:11 -0700

    fix script

--- kcdshop/post-set-playground.js
+++ kcdshop/post-set-playground.js
@@ -1,7 +1,16 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
-fs.writeFileSync(
-	path.join(process.env.KCDSHOP_PLAYGROUND_DEST_DIR, 'tsconfig.json'),
-	JSON.stringify({ extends: '../tsconfig' }, null, 2),
-)
+const exclude = [
+	'exercises/01.',
+	'exercises/02.',
+	'exercises/03.',
+	'exercises/04.',
+]
+
+if (exclude.every(e => !process.env.KCDSHOP_PLAYGROUND_SRC_DIR.includes(e))) {
+	fs.writeFileSync(
+		path.join(process.env.KCDSHOP_PLAYGROUND_DEST_DIR, 'tsconfig.json'),
+		JSON.stringify({ extends: '../tsconfig' }, null, 2),
+	)
+}
