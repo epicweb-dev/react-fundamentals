4e28dac838eb14d20c3e2190cb18f2cf763693ec
Creeland A. Provinsal <cree@provinsal.com>
2024-08-30 19:01:31 -0500

    small exercise updates (#373)

--- exercises/07.forms/01.problem.form/index.tsx
+++ exercises/07.forms/01.problem.form/index.tsx
@@ -4,6 +4,7 @@ function App() {
 	// 🐨 render a form
 	//   🐨 render a "Username" label
 	//   🐨 render an input with the name "username"
+	//   🐨 render a button
 	// 💯 associate the label to the input using htmlFor and id attributes
 	// 💯 explicitly set the button type appropriately
 	return 'TODO'
