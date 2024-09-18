5d6e3fa7d762131affb01c0dbaed1431fc1081e9
Kent C. Dodds <me+github@kentcdodds.com>
2024-09-18 16:06:32 -0600

    improve config

--- epicshop/playwright.config.js
+++ epicshop/playwright.config.js
@@ -3,7 +3,11 @@ import path from 'path'
 import { defineConfig, devices } from '@playwright/test'
 
 const PORT = process.env.PORT || '5639'
-const tmpDir = path.join(os.tmpdir(), 'epicreact-server-components')
+const tmpDir = path.join(
+	os.tmpdir(),
+	'epicshop-playwright',
+	path.basename(new URL('../', import.meta.url).pathname),
+)
 
 export default defineConfig({
 	outputDir: path.join(tmpDir, 'playwright-test-output'),
