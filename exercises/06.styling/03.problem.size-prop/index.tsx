63bd3166d7908200932a91b68886289011abafb3
Juanpa Diaz <juanpa.diaz108@gmail.com>
2025-06-12 22:31:59 -0500

    solution_06 - excercise 3

--- exercises/06.styling/03.problem.size-prop/index.tsx
+++ exercises/06.styling/03.problem.size-prop/index.tsx
@@ -1,20 +1,24 @@
 import { createRoot } from 'react-dom/client'
 
+type Size = 'samll' | 'medium' | 'large'
+
 export function Box({
 	// 💯 you can keep the style and className props here, but you can make this
 	// still work if you remove them. Give that a shot if you want.
 	style = {},
 	className = '',
 	// 🐨 add a size prop here
+	size,
 	...otherProps // 🦺 intersect (&) this with an object that has a size prop type here which is
 	// optional and is one of "small", "medium", or "large"
-}: React.ComponentProps<'div'>) {
+}: { size?: Size } & React.ComponentProps<'div'>) {
 	// 🐨 based on the size prop, define a new variable called sizeClassName
+	const sizeClassName = size ? `box--${size}` : ''
 	return (
 		<div
 			// 🐨 add the sizeClassName to the className prop
 			// 💯 for something extra, handle the case where no className is given (remove extra spaces).
-			className={`box ${className}`}
+			className={`box ${className} ${sizeClassName}`}
 			style={{ fontStyle: 'italic', ...style }}
 			{...otherProps}
 		/>
