e4d45e8341b94ea566ab06edb91acbbc21a60677
Juanpa Diaz <juanpa.diaz108@gmail.com>
2025-06-13 14:32:44 -0500

    solution_07 - excercise 3

--- exercises/07.forms/03.problem.types/index.tsx
+++ exercises/07.forms/03.problem.types/index.tsx
@@ -5,7 +5,27 @@ function App() {
 		<form action="api/onboarding">
 			<div>
 				<label htmlFor="usernameInput">Username:</label>
-				<input id="usernameInput" name="username" />
+				<input id="usernameInput" name="username" type="text" />
+			</div>
+			<div>
+				<label htmlFor="passwordInput">Password:</label>
+				<input id="passwordInput" name="password" type="password" />
+			</div>
+			<div>
+				<label htmlFor="ageInput">Age:</label>
+				<input id="ageInput" name="age" type="numbre" min={0} max={200} />
+			</div>
+			<div>
+				<label htmlFor="photoInput">Photo:</label>
+				<input id="photoInput" name="photo" type='file' accept="image/*" />
+			</div>
+			<div>
+				<label htmlFor="colorInput">Color:</label>
+				<input id="colorInput" name="color" type='color' />
+			</div>
+			<div>
+				<label htmlFor="startDateInput">StartDate:</label>
+				<input id="startDateInput" name="startDate" type='date' />
 			</div>
 			{/* 🐨 add appropriate inputs for:
 				- password
