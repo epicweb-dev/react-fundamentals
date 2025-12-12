24dc1c7f3aa49edd3c1659503708414a85c39500
Kent C. Dodds <me+github@kentcdodds.com>
2025-12-12 00:57:21 -0700

    update to epicshop CLI and v6.45.5

--- epicshop/setup-custom.js
+++ epicshop/setup-custom.js
@@ -1,10 +1,10 @@
 import path from 'node:path'
-import { warm } from '@epic-web/workshop-cli/warm'
 import {
 	getApps,
 	isProblemApp,
 	setPlayground,
 } from '@epic-web/workshop-utils/apps.server'
+import { warm } from 'epicshop/warm'
 import fsExtra from 'fs-extra'
 
 await warm()
